06c225c15844426b80d7919928328b59a167a099
MARIO EGUILUZ ALEBICTO <marioeguiluz@hotmail.com>
2018-02-07 18:25:46 +0000

    Initial Commit

---
+++ TheWeatherTurtle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheWeatherTurtle
+//
+//  Created by Mario Eguiluz on 07/02/2018.
+//  Copyright © 2018 Red Turtle Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
